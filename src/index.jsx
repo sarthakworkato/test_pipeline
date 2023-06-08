03d32469fde6115c612d4f65d960ebd525648332
sarthakworkato <sarthak.handa@workato.com>
2023-06-08 23:40:19 +0530

    api call for raw data

--- src/index.jsx
+++ src/index.jsx
@@ -1,51 +1,34 @@
-import ForgeUI, { render, Fragment, Text, IssuePanel, Button, Form, Select, Option } from '@forge/ui';
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Text, IssuePanel, useProductContext, useState } from "@forge/ui";
+
+const fetchMessageFromAPI = async () => {
+  const res = await api.fetch("https://tempapi.proj.me/api/bl2_94wyL", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+  return data;
+};
 
 const App = () => {
-  const handleSelectChange = (value) => {
-    console.log('Selected value:', value);
-    // Add your desired functionality herefr
-    if (value === 'o1') {
-      // Display nested dropdown for enrichments
-      return (
-        <Fragment>
-          <Select
-            label="Select an enrichment"
-            name="select-enrichment"
-            onChange={handleEnrichmentChange}>
-            <Option label="Enrichment 1" value="e1" />
-            <Option label="Enrichment 2" value="e2" />
-            <Option label="Enrichment 3" value="e3" />
-          </Select>
-          <Button text="Perform Enrichment" onClick={handleEnrichmentClick} />
-        </Fragment>
-      );
-    } else {
-      // Handle other options
-      console.log('Perform other action based on selected option');
-    }
-  };
+  const context = useProductContext();
+  const [responseData, setResponseData] = useState(null);
 
-  const handleEnrichmentChange = (value) => {
-    console.log('Selected enrichment:', value);
-    // Add your desired functionality here for the selected enrichment
-  };
+  useState(async () => {
+    const data = await fetchMessageFromAPI();
+    setResponseData(data);
+  }, []);
 
-  const handleEnrichmentClick = () => {
-    console.log('Perform Enrichment clicked!');
-    // Add your desired functionality here for performing the enrichment
-  };
+  if (responseData === null) {
+    return <Text>Loading...</Text>;
+  }
 
   return (
     <Fragment>
-      <Text>Incident Details</Text>
-      <Button text="Auto Respond to Incident" />
-      <Form>
-        <Select label="Select an option" name="select-option" onChange={handleSelectChange}>
-          <Option label="Enrich Incident" value="o1" />
-          <Option label="Perform Response Action" value="o2" />
-          <Option label="Assign Response Playbook" value="o3" />
-        </Select>
-      </Form>
+      <Text>SHA1: {responseData.SHA1}</Text>
     </Fragment>
   );
 };
